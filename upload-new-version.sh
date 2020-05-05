da15fe5981aef50582ec836de0c09f27a1d75af6
mluc <my.luc@opcity.com>
2020-05-04 22:23:44 -0500

    test

--- upload-new-version.sh
+++ upload-new-version.sh
@@ -1,25 +1,21 @@
 #!/bin/bash -e
 eksctl version
-pwd
-ls -la
 
-cd ~
-mkdir .aws
-touch .aws/credentials
-cat .aws/credentials
-echo "[default]" >> .aws/credentials
-echo "aws_access_key_id = $aws_access_key_id" >> .aws/credentials
-echo "aws_secret_access_key = $aws_secret_access_key" >> .aws/credentials
-cat .aws/credentials
-
-echo "$HOME"
+mkdir ~/.aws
+touch ~/.aws/credentials
+echo "[default]" >> ~/.aws/credentials
+echo "aws_access_key_id = $aws_access_key_id" >> ~/.aws/credentials
+echo "aws_secret_access_key = $aws_secret_access_key" >> ~/.aws/credentials
 
 pwd
 ls -la
-cd ..
-ls -la
 
-#eksctl get cluster prod2
+mkdir ~/.kube
+touch ~/.kube/config
+cp kube_config ~/.kube/config
+cat ~/.kube/config
+
+eksctl get cluster prod2
 
 #cat ~/.kube/config
 #/usr/local/bin/kubectl version
