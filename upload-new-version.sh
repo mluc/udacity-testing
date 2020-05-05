a2b39421d520b400f84d13dd8663003e448e7f5b
mluc <my.luc@opcity.com>
2020-05-04 22:03:07 -0500

    test

--- upload-new-version.sh
+++ upload-new-version.sh
@@ -1,13 +1,20 @@
 #!/bin/bash -e
 eksctl version
-mkdir ~/.aws
-touch ~/.aws/credentials
-cat ~/.aws/credentials
-echo "[default]" >> ~/.aws/credentials
-echo "aws_access_key_id = $aws_access_key_id" >> ~/.aws/credentials
-echo "aws_secret_access_key = $aws_secret_access_key" >> ~/.aws/credentials
-cat ~/.aws/credentials
+cd ~
+mkdir .aws
+touch .aws/credentials
+cat .aws/credentials
+echo "[default]" >> .aws/credentials
+echo "aws_access_key_id = $aws_access_key_id" >> .aws/credentials
+echo "aws_secret_access_key = $aws_secret_access_key" >> .aws/credentials
+cat .aws/credentials
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
 aws s3 ls
+
 eksctl get cluster prod2
 pwd
 ls -la
